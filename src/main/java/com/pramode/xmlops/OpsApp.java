5bfcb82d1791006dc9696f9a167584364f48626d
Pramode <Pramode@pandit-pramode>
2018-07-15 09:46:40 +0530

    ok

--- src/main/java/com/pramode/xmlops/OpsApp.java
+++ src/main/java/com/pramode/xmlops/OpsApp.java
@@ -3,18 +3,21 @@ package com.pramode.xmlops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
+//		@RestController -- > Return string json as ressponse
+//		@Controller     -- > Return view (static html and files ) as ressponse
 
-
-@RestController
+@Controller
 @SpringBootApplication
-public
 
- class OpsApp 
+public class OpsApp 
 {
 
 	public static void main(String[] args) {
@@ -22,10 +25,13 @@ public
 	}
 
 	
-	 @GetMapping("/")
-	  String home() {
-	        return "Spring Boot App is up now ";
-	    }
+	@GetMapping("/")
+//	@RequestMapping(method=RequestMethod.GET,value="/")
+	String home() {		    
+	      return "index.html";
+	}
+	 
+	 
 }
 
 
